3efcaee4635acd80d0cc8626f7d3fc715984b89f
Bismilah Langford <bismilah.lf@gmail.com>
2020-08-03 16:54:23 +0200

    Establish max health bars value

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
-//Definir variables para el valor de ataque y el valor máximo de vida que más adelante será definido por el usuario
+// Definir variables para el valor de ataque y el valor máximo de vida que más adelante será definido por el usuario
 const ATTACK_VALUE = 10;
 let chosenMaxLife = 100;
 
+// Establecer los valores máximos de las barras de salud
 
+adjustHealthBars(chosenMaxLife);
